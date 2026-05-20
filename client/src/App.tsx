ec5b78f0fbefea3d1622c9a24e663c98b4ff99ec
rspartangod <moibaldenegro.agentes@gmail.com>
2026-05-20 17:46:11 -0600

    Refactor routing to use routes array

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,26 @@
-import { Router, Route } from 'mouter-router';
-import LandingPage from './pages/LandingPage/LandingPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import AppDashboardPage from './pages/AppDashboardPage/AppDashboardPage';
-import DriverDashboardPage from './pages/DriverDashboardPage/DriverDashboardPage';
-import VehiclesPage from './pages/VehiclesPage/VehiclesPage';
-import TravelsPage from './pages/TravelsPage/TravelsPage';
-
-export default function App() {
-  return (
-    <Router>
-      <Route path="/" component={LandingPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/app" component={AppDashboardPage} />
-      <Route path="/driving" component={DriverDashboardPage} />
-      <Route path="/driving/vehicles" component={VehiclesPage} />
-      <Route path="/driving/travels" component={TravelsPage} />
-    </Router>
-  );
-}
+                                                                        
+    import { Router } from 'mouter-router';                                  
+    import LandingPage from './pages/LandingPage/LandingPage';               
+    import LoginPage from './pages/LoginPage/LoginPage';                     
+    import RegisterPage from './pages/RegisterPage/RegisterPage';            
+    import AppDashboardPage from './pages/AppDashboardPage/AppDashboardPage';
+    import DriverDashboardPage from '.                                       
+  /pages/DriverDashboardPage/DriverDashboardPage';                           
+    import VehiclesPage from './pages/VehiclesPage/VehiclesPage';            
+    import TravelsPage from './pages/TravelsPage/TravelsPage';               
+    import RoutesPage from './pages/RoutesPage/RoutesPage';                  
+                                                                             
+    const routes = [                                                         
+      { path: '/', component: LandingPage },                                 
+      { path: '/login', component: LoginPage },                              
+      { path: '/register', component: RegisterPage },                        
+      { path: '/app', component: AppDashboardPage },                         
+      { path: '/driving', component: DriverDashboardPage },
+      { path: '/driving/vehicles', component: VehiclesPage },
+      { path: '/driving/routes', component: RoutesPage },
+      { path: '/driving/travels', component: TravelsPage },
+    ];
+  
+    export default function App() {
+      return <Router routes={routes} />;
+    }
